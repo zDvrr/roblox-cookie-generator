053f6392dbba16fe85c4359b698a574860e68d57
zDvr <93467558+zDvrr@users.noreply.github.com>
2022-02-15 20:08:56 +0000

    Update requirements-install.bat

--- requirements-install.bat
+++ requirements-install.bat
@@ -1,13 +1,1 @@
-@echo off & cls
-title Roblox cookie generator
-set /p action= Install Requirements (y/n):
-if '%action%'=='y' goto requests
-of '%action%'=='n' goto start
-
-:requirements
-title Installing Requirements
 python -m pip install requests
-start
-
-
-:start
