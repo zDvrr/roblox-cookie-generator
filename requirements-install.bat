30a2d8fb6f359f2c050b3b6c8f16c6ee719e3044
zDvr <93467558+zDvrr@users.noreply.github.com>
2022-03-02 17:21:46 +0000

    Update requirements-install.bat

--- requirements-install.bat
+++ requirements-install.bat
@@ -1,2 +1,15 @@
 python -m pip install requests
 python -m pip install colorama
+python -m pip install plyer
+python -m pip install selenium
+python -m pip installbeautifulsoup4
+python -m pip install Pillow
+python -m pip install PyAutoGUI
+python -m pip install pyperclip
+python -m pip install selenium
+python -m pip install aiohttp
+python -m pip install pypresence
+python -m pip install pyinstaller
+python -m pip install dhooks
+python -m pip install lxml
+python -m pip install comtypes
